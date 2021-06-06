4059a4e31b7ce8087bb362983b5e13a95590b88c
NankuF <fireloki87@gmail.com>
2021-06-06 19:34:01 +0300

    added logic UserRegisterForm and customization him

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
@@ -24,4 +24,14 @@ def login(request):
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
