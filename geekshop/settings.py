5921831617f9f651e0ab7f59bbfa5f3d245c5b45
NankuF <fireloki87@gmail.com>
2021-06-09 12:01:22 +0300

    added handle of messages to templates

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -55,8 +55,8 @@ ROOT_URLCONF = 'geekshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': []
-        ,
+        'DIRS': [],
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
