915cdb32975391e6422c18e853dbfe1bc321ff08
NankuF <fireloki87@gmail.com>
2021-06-18 13:56:55 +0300

    create one big commit :(

--- baskets/admin.py
+++ baskets/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from baskets.models import Basket
+
+
+class BasketAdmin(admin.TabularInline):
+    """Позволяет показать корзину товара в классе User, в его профиле.
+    необходимо дописать атрибут в класс User!
+    необходим FK между моделями User и Basket
+    """
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
