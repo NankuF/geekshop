bcd29b48e2a0f66a03fc7b89a4241593f7845157
NankuF <fireloki87@gmail.com>
2021-06-21 10:21:54 +0300

    filter for products

--- products/views.py
+++ products/views.py
@@ -20,8 +20,12 @@ class ProductsListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductsListView, self).get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
         context.update({
             'title': 'Products',
             'categories': ProductCategory.objects.all(),
+            'category_id': category_id,
+            'filter_products_for_category_id': Product.objects.filter(category_id=category_id),
         })
         return context
+
